bab78d0e99b539b2b331d5b83bc105f5311a6246
John Evans <lgastako@gmail.com>
2012-06-11 16:28:02 -0700

    so far

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 
+import setuptools  # for side-effects to make 'python setup.py develop' work
 from setuptools import setup
+from setuptools import Command
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
 
 if __name__ == "__main__":
     setup(name="db",
@@ -8,4 +26,5 @@ if __name__ == "__main__":
           description="Queries for Humans",
           author="John Evans",
           author_email="lgastako@gmail.com",
-          provides="db")
+          provides="db",
+          cmdclass={"test": PyTest})
