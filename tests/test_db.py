4522ac0668fd7b85c5ad6b4fc4526d583009098f
John Evans <lgastako@gmail.com>
2012-06-17 20:54:30 -0700

    Move tests

---
+++ tests/test_db.py
@@ -0,0 +1,197 @@
+from functools import partial
+
+import sqlite3
+
+import pytest
+
+import db
+
+
+#########################
+# BASE TEST DEFINITIONS #
+#########################
+
+CREATE_FOO_SQL = """CREATE TABLE foo (
+                        foo_id INTEGER PRIMARY KEY,
+                        value TEXT
+                    )
+                 """
+
+
+class ExampleDBTests(object):
+
+    def setup_method(self, method):
+        db.drivers.clear()
+        self.conn = db.drivers.sqlite3x.connect(":memory:")
+        self.cursor = self.conn.cursor()
+        db.drivers.sqlite3x.register(":memory:")
+        db.do(CREATE_FOO_SQL)
+        db.do("INSERT INTO foo VALUES (1, 'foo')")
+
+    def insert_another(self):
+        db.do("INSERT INTO foo (foo_id, value) VALUES (2, 'bar')")
+
+
+class DoTests(ExampleDBTests):
+
+    def test_insert(self):
+        db.do("DELETE FROM foo")
+        db.do("INSERT INTO foo (foo_id, value) VALUES (1, 'baz')")
+
+        rows = self.items("SELECT * FROM foo")
+        assert len(rows) == 1
+        row = rows[0]
+        assert row.foo_id == 1
+        assert row.value == 'baz'
+
+
+class ItemTests(ExampleDBTests):
+
+    def test_basic_select(self):
+        row = self.item("SELECT * FROM foo")
+
+        assert row.foo_id == 1
+        assert row.value == 'foo'
+
+
+class ItemsTests(ExampleDBTests):
+
+    def test_basic_select(self):
+        rows = self.items("SELECT * FROM foo")
+
+        assert len(rows) == 1
+        row = rows[0]
+        assert row.foo_id == 1
+        assert row.value == 'foo'
+
+    def test_basic_select_multiple(self):
+        self.insert_another()
+
+        rows = self.items("SELECT * FROM foo")
+
+        assert len(rows) == 2
+        row = rows[0]
+        assert row.foo_id == 1
+        assert row.value == 'foo'
+        row = rows[1]
+        assert row.foo_id == 2
+        assert row.value == 'bar'
+
+
+class CountTests(ExampleDBTests):
+
+    def test_most_basic(self):
+        assert self.count("foo") == 1
+
+    def test_basic_count_multiple(self):
+        self.insert_another()
+        assert self.count("foo") == 2
+
+    def test_complex_count(self):
+        db.do("""CREATE TABLE bar (
+                     bar_id INTEGER PRIMARY KEY,
+                     value TEXT
+                 )""")
+        db.do("INSERT INTO foo VALUES (2, 'bar')")
+        db.do("INSERT INTO foo VALUES (3, 'baz')")
+        db.do("INSERT INTO foo VALUES (4, 'bim')")
+
+        db.do("INSERT INTO bar VALUES (1, 'foo')")
+        db.do("INSERT INTO bar VALUES (2, 'bart')")
+        db.do("INSERT INTO bar VALUES (3, 'bazzle')")
+        db.do("INSERT INTO bar VALUES (4, 'bim')")
+
+        assert self.count("foo, bar WHERE foo.value = bar.value") == 2
+
+
+############
+# CONTEXTS #
+############
+
+
+class ExplicitConnection(object):
+
+    def setup_method(self, method):
+        super(ExplicitConnection, self).setup_method(method)
+        self.do = partial(db.do, _conn=self.conn)
+        self.item = partial(db.item, _conn=self.conn)
+        self.items = partial(db.items, _conn=self.conn)
+        self.count = partial(db.count, _conn=self.conn)
+
+
+class ImplicitConnection(object):
+
+    def setup_method(self, method):
+        super(ImplicitConnection, self).setup_method(method)
+        self.do = db.do
+        self.item = db.item
+        self.items = db.items
+        self.count = db.count
+
+
+######################################
+# COMBINATIONS OF TESTS AND CONTEXTS #
+######################################
+
+
+class TestDoExplicit(ExplicitConnection, DoTests):
+    pass
+
+
+class TestDoImplicit(ImplicitConnection, DoTests):
+    pass
+
+
+class TestItemExplicit(ExplicitConnection, ItemTests):
+    pass
+
+
+class TestItemImplicit(ImplicitConnection, ItemTests):
+    pass
+
+
+class TestItemsExplicit(ExplicitConnection, ItemsTests):
+    pass
+
+
+class TestItemsImplicit(ImplicitConnection, ItemsTests):
+    pass
+
+
+class TestCountExplicit(ExplicitConnection, CountTests):
+    pass
+
+
+class TestCountImplicit(ImplicitConnection, CountTests):
+    pass
+
+
+#########################
+# ONE-OFF / OTHER TESTS #
+#########################
+
+class TestMultipleDatabases(ExampleDBTests):
+
+    def test_create_and_connect_to_two_separately(self):
+        conn1 = db.drivers.sqlite3x.connect(":memory:")
+        conn2 = db.drivers.sqlite3x.connect(":memory:")
+
+        db.drivers.register(lambda *a, **k: conn1, "manual1")
+        db.drivers.register(lambda *a, **k: conn2, "manual2")
+
+        db1 = db.get("manual1")
+        db2 = db.get("manual2")
+
+        db1.do(CREATE_FOO_SQL)
+        db2.do(CREATE_FOO_SQL)
+
+        db1.do("INSERT INTO foo (value) VALUES (1)")
+        db1.do("INSERT INTO foo (value) VALUES (2)")
+        db1.do("INSERT INTO foo (value) VALUES (3)")
+
+        db2.do("INSERT INTO foo (value) VALUES (4)")
+        db2.do("INSERT INTO foo (value) VALUES (5)")
+        db2.do("INSERT INTO foo (value) VALUES (6)")
+
+        assert db1.item("SELECT SUM(value) AS n FROM foo").n == 6
+        assert db2.item("SELECT SUM(value) AS n FROM foo").n == 15
---
+++ tests/test_drivers.py
@@ -0,0 +1,145 @@
+import pytest
+import sqlite3
+
+import db
+
+
+class DriverTests(object):
+
+    def clear_drivers(self):
+        db.drivers._DRIVERS = {}
+
+    def make_driver(self, label):
+        invocations = []
+
+        def driver(*args, **kwargs):
+            invocation = (label, args, kwargs)
+            invocations.append(invocation)
+            return invocation
+
+        driver.invocations = invocations
+        return driver
+
+    def install_one_driver(self):
+        db.drivers.clear()
+        db.drivers.register(self.make_driver("connect_one_only"), "only")
+
+    def install_two_drivers(self, second_driver_name="DEFAULT"):
+        db.drivers.clear()
+        db.drivers.register(self.make_driver("connect_two_first"), "first"),
+        db.drivers.register(self.make_driver(second_driver_name),
+                            second_driver_name)
+
+
+class TestExpandDriverName(DriverTests):
+
+    def test_not_none_non_existent(self):
+        assert db.drivers.expand_name("bar") == "bar"
+
+    def test_not_none_single_driver(self):
+        self.install_one_driver()
+        assert db.drivers.expand_name("foo") == "foo"
+
+    def test_not_none_multiple_drivers_default_exists(self):
+        self.install_two_drivers()
+        assert db.drivers.expand_name("foo") == "foo"
+
+    def test_none_single_driver(self):
+        self.install_one_driver()
+        assert db.drivers.expand_name(None) == "only"
+
+    def test_none_multiple_drivers_default_exists(self):
+        self.install_two_drivers()
+        assert db.drivers.expand_name(None) == "DEFAULT"
+
+    def test_none_multiple_drivers_no_default_exists(self):
+        self.install_two_drivers("bar")
+        assert db.drivers.expand_name(None) == "DEFAULT"
+
+
+class TestCount(DriverTests):
+
+    def test_none(self):
+        self.clear_drivers()
+        assert db.drivers.count() == 0
+
+    def test_one(self):
+        self.install_one_driver()
+        assert db.drivers.count() == 1
+
+    def test_two(self):
+        self.install_two_drivers()
+        assert db.drivers.count() == 2
+
+
+class TestClear(DriverTests):
+
+    def test_already_clear(self):
+        self.clear_drivers()
+        db.drivers.clear()
+        assert db.drivers.count() == 0
+
+    def test_not_already_clear(self):
+        self.install_one_driver()
+        db.drivers.clear()
+        assert db.drivers.count() == 0
+
+    def test_not_already_clear_multiple(self):
+        self.install_two_drivers()
+        db.drivers.clear()
+        assert db.drivers.count() == 0
+
+
+class TestConnect(DriverTests):
+
+    def test_connect_default_one_driver(self):
+        self.install_one_driver()
+        assert db.drivers.connect() == ("connect_one_only", (), {})
+
+    def test_connect_default_two_drivers(self):
+        self.install_two_drivers()
+        assert db.drivers.connect() == ("DEFAULT", (), {})
+
+    def test_connect_non_default(self):
+        self.install_two_drivers()
+        assert db.drivers.connect("first") == ("connect_two_first", (), {})
+
+
+class TestDisconnect(DriverTests):
+
+    def test_disconnect_default_one_driver(self):
+        self.install_one_driver()
+        db.drivers.disconnect("foo")
+        assert db.drivers._DRIVERS["only"].invocations == \
+            [("connect_one_only", ("foo",), {})]
+
+    def test_disconnect_default_two_drivers(self):
+        self.install_two_drivers()
+        db.drivers.disconnect("foo")
+        assert db.drivers._DRIVERS["DEFAULT"].invocations == \
+            [("DEFAULT", ("foo",), {})]
+
+    def test_disconnect_non_default(self):
+        self.install_two_drivers()
+        db.drivers.disconnect("foo", "first")
+        assert db.drivers._DRIVERS["first"].invocations == \
+            [("connect_two_first", ("foo",), {})]
+
+
+class TestMisc(DriverTests):
+
+    def test_registering_returns_db(self):
+        conn = db.drivers.sqlite3x.connect(":memory:")
+        test_db = db.drivers.register(lambda *a, **kw: conn, "test_db")
+
+        assert test_db is not None
+
+        test_db.do("""CREATE TABLE foo (
+            foo_id INTEGER PRIMARY KEY,
+            bar TEXT NOT NULL
+        )""")
+        test_db.do("INSERT INTO foo (foo_id, bar) VALUES (1, 'a')")
+        test_db.do("INSERT INTO foo (foo_id, bar) VALUES (2, 'b')")
+        test_db.do("INSERT INTO foo (foo_id, bar) VALUES (3, 'c')")
+
+        assert test_db.item("SELECT COUNT(*) AS n FROM foo").n == 3
