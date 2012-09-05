3b029bd531af825cd404ce22fa2a8c510b8c305f
John Evans <lgastako@gmail.com>
2012-09-04 20:10:08 -0700

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
-with open('requirements.txt') as f:
+
+with open("requirements.txt") as f:
     requirements = [req.strip() for req in f]
 
 
 if __name__ == "__main__":
     setup(name="db",
-          version=os.environ.get('MILOVERSION'),
+          version="0.0.2",
           description="Databases for Humans",
           author="John Evans",
           author_email="lgastako@gmail.com",
