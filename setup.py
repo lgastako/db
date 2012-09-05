bd1c1dbd8ff29b399ad11b9047d9e06f00d64dfa
John Evans <lgastako@gmail.com>
2012-09-05 16:09:55 -0700

    Fixed requirements.

--- setup.py
+++ setup.py
@@ -5,16 +5,12 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-with open("requirements.txt") as f:
-    requirements = [req.strip() for req in f]
-
-
 if __name__ == "__main__":
     setup(name="db",
           version="0.0.2",
           description="Databases for Humans",
           author="John Evans",
           author_email="lgastako@gmail.com",
-          install_requires=requirements,
+          install_requires=["antiorm"],
           packages=find_packages(),
           provides=["db"])
